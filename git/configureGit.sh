cdfdcbcd47ac73805c288cb356e19519b4756b8d
MichiTheGenius <litzinger04@gmail.com>
2022-04-28 20:01:51 +0200

    removed install git command from git config script
    you needed git in order to clone the dotfiles repo beforehand
    installing it again makes no sense
    added a few comments as well to explain what the script does

--- git/configureGit.sh
+++ git/configureGit.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/bash
 
-sudo apt update
-sudo apt install git
-
 # configure git
 
 git config --global user.name "MichiTheGenius"
@@ -15,10 +12,12 @@ cp .gitconfig ~/
 echo 'Name your ssh key file git_ssh'
 ssh-keygen -t ed25519 -C "litzinger04@gmail.com"
 
+# start ssh-agent
 eval "$(ssh-agent -s)"
 
 ssh-add ~/.ssh/git_ssh
 
+# add the line to the file that it remembers the user
 echo 'IdentityFile ~/.ssh/git_ssh' >> ~/.ssh/config
 
 echo 'eval "$(ssh-agent -s)"' >> ~/.bashrc
