40358688e16a9df12d71be06da390d5697626383
MichiTheGenius <litzinger04@gmail.com>
2022-09-13 21:18:06 +0200

    added hack font to setup fonts script

--- fonts/setupFonts.sh
+++ fonts/setupFonts.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-sudo cp -r JetBrainsNerd/ /usr/share/fonts
-sudo fc-cache -fv
+# the first command line argument is the font we want
+FONT=$1
+INSTALL_PATH="/usr/share/fonts"
+
+if [ $FONT = "jet" ]
+then
+	echo "installing JetBrainsMono NerdFont to $INSTALL_PATH"
+	sudo cp -r JetBrainsNerd/ $INSTALL_PATH
+fi
+
+if [ $FONT = "hack" ]
+then
+	echo "installing Hack NerdFont to $INSTALL_PATH"
+	sudo cp -r HackNerdFont/ $INSTALL_PATH
+fi
+sudo fc-cache -f
+
+echo "find the font with the 'fc-list | grep FONT_NAME' command"
